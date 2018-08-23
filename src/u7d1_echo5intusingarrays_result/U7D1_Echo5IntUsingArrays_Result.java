9d89413afb0450404b975b0faa2e1961bc7e9d0a
Paulino <paulino.villarreal@gmail.com>
2018-08-23 10:20:58 -0500

    Migrating files to this repo

---
+++ src/u7d1_echo5intusingarrays_result/U7D1_Echo5IntUsingArrays_Result.java
@@ -0,0 +1,22 @@
+
+package u7d1_echo5intusingarrays_result;
+
+import java.util.Scanner;
+
+public class U7D1_Echo5IntUsingArrays_Result {
+
+    public static void main(String[] args) {
+        System.out.println("Please Enter 5 Integers: ");
+        Scanner input = new Scanner(System.in);
+        
+        int[] myList = new int[5];
+        
+        
+        
+        
+        for (int i = 0; i < 5; int++){
+            
+        }
+    }
+    
+}
